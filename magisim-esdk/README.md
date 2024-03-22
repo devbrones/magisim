2d388926d2d5477547f4d9000a1f901ae221b604
devbrones <jonas.cronholm@protonmail.com>
2024-03-22 10:02:26 +0100

    cleaned the repo

--- magisim-esdk/README.md
+++
@@ -1,2 +0,0 @@
-<p><img align="right" src="./resources/magisim_logo256.png" width=60></p>
-<h1>magisim for developers</h1>
--- magisim-esdk/src/tension.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-import importlib
-import yaml
-import json
-import logging
-from pathlib import Path
-
-
